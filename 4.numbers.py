75b0d61ff10eb8f9f46d305440c500bfeb8b5071
César <cesarlpb@gmail.com>
2022-11-28 10:02:40 +0100

    numbers: complex, casting y random

--- 4.numbers.py
+++ 4.numbers.py
@@ -23,7 +23,25 @@ print(round(cociente, 5) == round(2.0/6.0 * 1e6, 5))    # había una discrepanci
                                                         # redondeamos a 5 decimales para poder comparar
 print(f"El Sol tiene {int(cociente)} de veces más masa que la Tierra")
 
-# complex
+#%% complex
+z1 = 2 + 1j
+z2 = 1 + 1j
+print(f"Suma: {z1 + z2}")
+print(f"Resta: {z1 - z2}")
+print(f"División: {z1 * z2}")   # El cálculo de prod y división de complejos difiere
+print(f"Suma: {z1 / z2}")       # de las operaciones con números reales en general
 
-# random
+#%% Casting
+my_int = int(3.1415)
+my_float = float(25)
+my_complex = complex(1)
+
+print(my_int, type(my_int).__name__)
+print(my_float, type(my_float).__name__)
+print(my_complex, type(my_complex).__name__)
+#%% random
+import random
+for i in range(1,4):
+    print(random.randrange(1, 100))
+
 # %%
