277601580dad8f25af30a83a22213ca5b8c91f8d
César <cesarlpb@gmail.com>
2022-11-30 11:48:18 +0100

    Cálculo de impuestos por tramos

--- ejercicios/1-15.beginners.py
+++ ejercicios/1-15.beginners.py
@@ -144,3 +144,31 @@ def invertir_num(num):
 
 print(invertir_num(7536))
 print(invertir_num(1005))
+
+#%% 12 - Cálculo de tramos de impuestos
+
+def calc_taxes(num):
+    # porcentajes de cada tramo
+    tramos = [0, 10, 20]
+    
+    if num > 10000 and num < 20000:
+        cantidades = [10000, num-10000, 0]
+    elif num > 20000:
+        cantidades = [10000, 10000, num-20000]
+    else:
+        cantidades = [num, 0, 0]
+
+    # cantidades tiene el mismo tamaño
+    # que tramos
+    idx = 0
+    suma = 0
+    while idx < len(cantidades):
+        cantidades[idx] = cantidades[idx] * tramos[idx]/100
+        suma += cantidades[idx]
+        idx += 1
+
+    return (cantidades, suma)
+
+print(calc_taxes(9999))
+print(calc_taxes(15000))
+print(calc_taxes(45000))
