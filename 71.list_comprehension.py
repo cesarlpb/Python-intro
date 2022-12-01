a57a88e64af13d29a2674eb27672dbfee455b7fc
César <cesarlpb@gmail.com>
2022-12-01 09:16:44 +0100

    List comprehension edit

--- 71.list_comprehension.py
+++ 71.list_comprehension.py
@@ -17,3 +17,36 @@ print("for:", newlist)
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = [el for el in fruits if "a" in el]
 print("list comprehension:", newlist)
+
+#%% Otro ejemplo
+
+# Lista sin "apple"
+newlist = [x for x in fruits if x != "apple"]
+print("list comprehension:", newlist)
+
+#%% Si omitimos condición final iteramos todos los elementos
+
+newlist = [x for x in fruits]
+print("nueva lista:", newlist)
+
+#%% Listado de números con range
+
+newlist = [x for x in range(1, 11)]
+print("nueva lista:", newlist)
+
+#%% Añadiendo una condición al rango
+
+newlist = [x for x in range(10) if x < 5]
+print("nueva lista:", newlist)
+
+#%% Aplicando método de string en expresión
+
+newlist = [x.upper() for x in fruits]
+print("nueva lista:", newlist)
+
+#%% Colocar todos los valores constantes en lista
+newlist = ['x' for x in fruits]
+print("nueva lista:", newlist)
+#%% Condición inicial más compleja
+newlist = [x if x != "banana" else "new_banana" for x in fruits]
+print("nueva lista:", newlist)
